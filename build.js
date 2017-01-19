7bdc0ea2f9e32141f18857a84b6eee813723e5d0
Lionel Tay <lionel.tay@hotmail.com>
2017-01-19 19:47:30 +1100

    initial commit

---
+++ build.js
@@ -0,0 +1,40 @@
+const exec = require('child_process').exec
+const fs = require('fs')
+const ncp = require('ncp').ncp
+const shell = require('shelljs')
+const path = require('path')
+ncp.limit = 16
+
+const apps = [
+	'MathSite',
+]
+
+console.log('Building and collecting apps')
+
+apps.forEach(app => {
+	exec(`cd ${app} && npm run serverbuild`, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`)
+			return
+		}
+		
+		console.log(`${app} build complete`)
+		
+		copyDistToServer(app)
+	})
+})
+
+
+function copyDistToServer(appName) {
+	const dir = `./Server/src/public/${appName}/dist`
+	if (!fs.existsSync(dir)) {
+		shell.mkdir('-p', path.join(__dirname, dir))
+	}
+	
+	ncp(`./${appName}/dist`, dir, function (err) {
+	 if (err) {
+		 return console.error(err)
+	 }
+	 console.log(`${appName} copy to server complete`)
+	})
+}
